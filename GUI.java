ace365592de7f1c931810e77d4bbf07427575828
Clara Delfino <22550453+claradelfino@users.noreply.github.com>
2023-02-28 14:49:57 +0100

    test

--- GUI.java
+++ GUI.java
@@ -4,9 +4,7 @@ import javax.swing.JFrame;
 
 public class GUI extends JFrame {
 	
-public GUI () {
-
-
+public GUI () 
 	setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 	this.setContentPane();
 	this.setVisible(true);
